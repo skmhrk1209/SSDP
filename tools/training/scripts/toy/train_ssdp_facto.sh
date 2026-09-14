76ea54d7b926ccdaea8afaaa245afc030c70c557
Hiroki Sakuma <kmhskm0720@gmail.com>
2026-09-15 02:25:08 +0900

    update

--- tools/training/scripts/toy/train_ssdp_facto.sh
+++ tools/training/scripts/toy/train_ssdp_facto.sh
@@ -5,5 +5,6 @@ uv run python tools/training/launch.py -m \
     experiment_name='${method_name}-${scheduler.params.scene_id}' \
     scheduler.walltime=01:15:00 \
     scheduler.params.scene_id=k1_w0.05,k2_w0.05,k3_w0.05,k1_w0.025,k2_w0.025,k3_w0.025,k1_w0.0125,k2_w0.0125,k3_w0.0125 \
+    scheduler.params.deterministic_end_ratio=0.25 \
     scheduler.params.uniform_sampling=true \
     timestamp=202604 \
--- tools/training/scripts/toy/train_ssdp_facto_survival_approx_v2.sh
+++ tools/training/scripts/toy/train_ssdp_facto_survival_approx_v2.sh
@@ -6,8 +6,8 @@ uv run python tools/training/launch.py -m \
     scheduler.walltime=01:30:00 \
     scheduler.params.scene_id=k1_w0.05,k2_w0.05,k3_w0.05,k1_w0.025,k2_w0.025,k3_w0.025,k1_w0.0125,k2_w0.0125,k3_w0.0125 \
     scheduler.params.deterministic_end_ratio=0.25 \
-    scheduler.params.up_cross_approx_end_ratio=0.5 \
-    scheduler.params.up_cross_anneal_end_ratio=1.0 \
     scheduler.params.survival_approx_end_ratio=0.5 \
+    scheduler.params.up_cross_approx_end_ratio=0.5 \
+    scheduler.params.up_cross_anneal_end_ratio=0.75 \
     scheduler.params.uniform_sampling=true \
     timestamp=202604 \
